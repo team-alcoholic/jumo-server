53ca45a06bf4f268faf22f5ccd190273834bdf48
dongsuk <scienceyds@naver.com>
2024-07-10 15:01:30 +0900

    feat: Spring Security에 OAuth2UserService 적용

--- src/main/java/team_alcoholic/jumo_server/config/SecurityConfig.java
+++ src/main/java/team_alcoholic/jumo_server/config/SecurityConfig.java
@@ -2,11 +2,16 @@ package team_alcoholic.jumo_server.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import team_alcoholic.jumo_server.auth.service.OAuth2UserService;
 
+/**
+ * Spring Security 설정 클래스
+ */
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
@@ -14,22 +19,26 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
+        /*
+
+         */
         http
-                .csrf((csrf) -> csrf.disable());
+                .csrf(csrf -> csrf.disable())
+                .formLogin(login -> login.disable())
+                .httpBasic(basic -> basic.disable())
+                .oauth2Login((oauth2) -> oauth2
+                        .userInfoEndpoint((userInfoEndpointConfig) -> userInfoEndpointConfig
+                                .userService(new OAuth2UserService())
+                        )
+                );
 
         http
-                .formLogin((login) -> login.disable());
-
-        http
-                .httpBasic((basic) -> basic.disable());
-
-        http
-                .oauth2Login(Customizer.withDefaults());
-
-        http
-                .authorizeHttpRequests((auth) -> auth
+                .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/", "/oauth2/**", "/login/**").permitAll()
                         .anyRequest().authenticated());
+//                .exceptionHandling(exceptionHandling -> exceptionHandling
+//                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+//                );
 
         return http.build();
     }
