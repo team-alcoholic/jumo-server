a1d6e0bde0e0255293fc16991380053e5ca0e5da
yangdongsuk <51476641+yangdongsuk@users.noreply.github.com>
2024-07-12 15:01:03 +0900

    [BYOB-48] 회원가입 API + 사용자 DB (#4)
    * chore: spring data jpa 사용
    
    * feat: OAuth2 사용자 정보를 가져와 UserService로 넘겨 사용자 정보를 조회하거나 생성
    
    * feat: UserController 구현
    
    * feat: User 괸리하는 기능 구현
    
    * refactor: userDTO 기반으로 세션 관리하도록 변경
    
    * refactor: OAuth2UserService를 의존성 주입으로 변경

--- src/main/java/team_alcoholic/jumo_server/config/SecurityConfig.java
+++ src/main/java/team_alcoholic/jumo_server/config/SecurityConfig.java
@@ -1,20 +1,24 @@
 package team_alcoholic.jumo_server.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import team_alcoholic.jumo_server.user.repository.UserRepository;
 import team_alcoholic.jumo_server.auth.service.OAuth2UserService;
+import team_alcoholic.jumo_server.user.service.UserService;
 
 /**
  * Spring Security 설정 클래스
  */
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final OAuth2UserService oAuth2UserService;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -28,7 +32,7 @@ public class SecurityConfig {
                 .httpBasic(basic -> basic.disable())
                 .oauth2Login((oauth2) -> oauth2
                         .userInfoEndpoint((userInfoEndpointConfig) -> userInfoEndpointConfig
-                                .userService(new OAuth2UserService())
+                                .userService(oAuth2UserService)
                         )
                 );
 
