304106be32b5387cd33fe8ab054d8dc7f4e8a2e0
dongsuk <scienceyds@naver.com>
2024-07-15 23:20:04 +0900

    fix: 깃 WebConfig 충돌 해결

--- src/main/java/team_alcoholic/jumo_server/config/WebConfig.java
+++ src/main/java/team_alcoholic/jumo_server/config/WebConfig.java
@@ -10,9 +10,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:3000")
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-            .allowedHeaders("*")
-            .allowCredentials(true);
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("Authorization", "Content-Type")
+                .exposedHeaders("Custom-Header")
+                .allowCredentials(true)
+                .maxAge(3600);
     }
 }
