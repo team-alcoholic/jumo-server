7317b430d446e68da4398b0ac646914beef919e6
yangdongsuk <51476641+yangdongsuk@users.noreply.github.com>
2024-07-15 17:21:13 +0900

    [BYOB-53] 세션 관리 구현 + 로그아웃 API (#7)
    * release/1.0.0에 develop 머지하기 (#6)
    
    * [BYOB-48] 회원가입 API + 사용자 DB (#4)
    
    * chore: spring data jpa 사용
    
    * feat: OAuth2 사용자 정보를 가져와 UserService로 넘겨 사용자 정보를 조회하거나 생성
    
    * feat: UserController 구현
    
    * feat: User 괸리하는 기능 구현
    
    * refactor: userDTO 기반으로 세션 관리하도록 변경
    
    * refactor: OAuth2UserService를 의존성 주입으로 변경
    
    * [BYOB-70] CORS 설정 (#5)
    
    ---------
    
    Co-authored-by: Youngmin Song <68019733+y-ngm-n@users.noreply.github.com>
    
    * feat: 로그인 완료시 메인 페이지로 이동하도록 설점
    
    * feat: 인가되지 않은 주소 접근시 보낼 json 생성하는 객체 구현
    
    * feat: 스프링 세큐리티에 미인가 요청 처리 객체 적용
    
    * fix: 안 쓰는 exception 제거
    
    * chore: redis 드라이버 추가
    
    * chore: 세션 관련 redis 드라이버 추가
    
    * chore: 세션 관련 redis 드라이버 비활성화
    
    * feat: 성공 url과 로그아웃 설정 구현
    
    * feat: 프론트 개발을 위한 cors 설정
    
    ---------
    
    Co-authored-by: Youngmin Song <68019733+y-ngm-n@users.noreply.github.com>

--- src/main/java/team_alcoholic/jumo_server/config/SecurityConfig.java
+++ src/main/java/team_alcoholic/jumo_server/config/SecurityConfig.java
@@ -10,40 +10,48 @@ import team_alcoholic.jumo_server.user.repository.UserRepository;
 import team_alcoholic.jumo_server.auth.service.OAuth2UserService;
 import team_alcoholic.jumo_server.user.service.UserService;
 
-/**
- * Spring Security 설정 클래스
- */
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
     private final OAuth2UserService oAuth2UserService;
+    private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
+
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
-        /*
-
-         */
         http
                 .csrf(csrf -> csrf.disable())
                 .formLogin(login -> login.disable())
                 .httpBasic(basic -> basic.disable())
-                .oauth2Login((oauth2) -> oauth2
-                        .userInfoEndpoint((userInfoEndpointConfig) -> userInfoEndpointConfig
+                .oauth2Login(oauth2 -> oauth2
+                        .userInfoEndpoint(userInfoEndpointConfig -> userInfoEndpointConfig
+
                                 .userService(oAuth2UserService)
                         )
+                        .successHandler((request, response, authentication) -> {
+                            response.sendRedirect("http://localhost:3000");
+                        })
+                )
+                .exceptionHandling(exceptionHandling -> exceptionHandling
+                        .authenticationEntryPoint(customAuthenticationEntryPoint)
+                )
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("http://localhost:3000/")
+                        .invalidateHttpSession(true)
+                        .deleteCookies("JSESSIONID")
                 );
 
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/", "/oauth2/**", "/login/**").permitAll()
-                        .anyRequest().authenticated());
-//                .exceptionHandling(exceptionHandling -> exceptionHandling
-//                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
-//                );
+                        .requestMatchers("/", "/oauth2/**", "/login/**", "/logout").permitAll()
+                        .anyRequest().authenticated()
+                );
 
         return http.build();
     }
+
+
 }
