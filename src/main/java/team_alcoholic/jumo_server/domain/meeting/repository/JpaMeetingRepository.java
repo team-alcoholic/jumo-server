fc07a0df1744102abcb789fe3b16561203c7ed2d
y-ngm-n <wogitopp@naver.com>
2024-07-24 11:02:48 +0900

    fix: Meeting repository에서 패키지 구조 변경에 따라 발생한 JPQL 오류 해결

--- src/main/java/team_alcoholic/jumo_server/domain/meeting/repository/JpaMeetingRepository.java
+++ src/main/java/team_alcoholic/jumo_server/domain/meeting/repository/JpaMeetingRepository.java
@@ -30,14 +30,14 @@ public class JpaMeetingRepository implements MeetingRepository{
 
     @Override
     public List<MeetingListDto> findLatestMeetingList(int limit) {
-        return em.createQuery("select new team_alcoholic.jumo_server.meeting.dto.MeetingListDto(m.id, m.uuid, m.name, m.status, m.meetingAt, m.fixAt, r.admnm, m.liquors, m.participatesMin, m.participatesMax, m.payment, m.byob, m.thumbnailImage, m.externalService) from Meeting m, Region r where m.region=r.admcd order by m.id desc limit :limit", MeetingListDto.class)
+        return em.createQuery("select new team_alcoholic.jumo_server.domain.meeting.dto.MeetingListDto(m.id, m.uuid, m.name, m.status, m.meetingAt, m.fixAt, r.admnm, m.liquors, m.participatesMin, m.participatesMax, m.payment, m.byob, m.thumbnailImage, m.externalService) from Meeting m, Region r where m.region=r.admcd order by m.id desc limit :limit", MeetingListDto.class)
             .setParameter("limit", limit)
             .getResultList();
     }
 
     @Override
     public List<MeetingListDto> findLatestMeetingListById(int limit, Long cursor) {
-        return em.createQuery("select new team_alcoholic.jumo_server.meeting.dto.MeetingListDto(m.id, m.uuid, m.name, m.status, m.meetingAt, m.fixAt, r.admnm, m.liquors, m.participatesMin, m.participatesMax, m.payment, m.byob, m.thumbnailImage, m.externalService) from Meeting m, Region r where m.region=r.admcd and m.id<:cursor order by m.id desc limit :limit", MeetingListDto.class)
+        return em.createQuery("select new team_alcoholic.jumo_server.domain.meeting.dto.MeetingListDto(m.id, m.uuid, m.name, m.status, m.meetingAt, m.fixAt, r.admnm, m.liquors, m.participatesMin, m.participatesMax, m.payment, m.byob, m.thumbnailImage, m.externalService) from Meeting m, Region r where m.region=r.admcd and m.id<:cursor order by m.id desc limit :limit", MeetingListDto.class)
             .setParameter("limit", limit)
             .setParameter("cursor", cursor)
             .getResultList();
