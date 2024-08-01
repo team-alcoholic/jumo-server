dcb290cb30fe80001217bb0cb80e27ec7d4a55c4
dongsuk <scienceyds@naver.com>
2024-08-01 17:26:10 +0900

    feat: 공통 notfound 에러 익셉션

---
+++ src/main/java/team_alcoholic/jumo_server/global/error/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package team_alcoholic.jumo_server.global.error.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
