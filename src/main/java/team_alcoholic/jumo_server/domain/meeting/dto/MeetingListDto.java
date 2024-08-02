e267c0a6f23a7cb254d693bd6f682126503b561d
yangdongsuk <51476641+yangdongsuk@users.noreply.github.com>
2024-08-02 11:22:29 +0900

    [BYOB-136] 목록 조회 버그 해결 (#31)
    * fix: region null 문제 해결을 위해 스프링데이터JPA로 변경

--- src/main/java/team_alcoholic/jumo_server/domain/meeting/dto/MeetingListDto.java
+++ src/main/java/team_alcoholic/jumo_server/domain/meeting/dto/MeetingListDto.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import team_alcoholic.jumo_server.domain.meeting.domain.Meeting;
+import team_alcoholic.jumo_server.domain.region.domain.Region;
 
 import java.time.LocalDateTime;
 
@@ -14,20 +15,20 @@ public class MeetingListDto {
 
     private Long id;
     private String uuid;
-//    private String host;
+    //    private String host;
     private String name;
     private String status;
     private LocalDateTime meetingAt;
     private LocalDateTime fixAt;
     private String region;
-//    private String place;
+    //    private String place;
     private String liquors;
     private Integer participatesMin;
     private Integer participatesMax;
     private Integer payment;
-//    private String paymentMethod;
+    //    private String paymentMethod;
     private boolean byob;
-//    private Integer byobMin;
+    //    private Integer byobMin;
 //    private Integer byobMax;
 //    private String description;
     private String thumbnail;
@@ -41,7 +42,10 @@ public class MeetingListDto {
         this.status = meeting.getStatus();
         this.meetingAt = meeting.getMeetingAt();
         this.fixAt = meeting.getFixAt();
-        this.region = meeting.getRegion();
+        if (meeting.getRegion() != null) {
+            this.region = meeting.getRegion().getAdmnm();
+        }
+
         this.liquors = meeting.getLiquors();
         this.participatesMin = meeting.getParticipatesMin();
         this.participatesMax = meeting.getParticipatesMax();
@@ -50,4 +54,6 @@ public class MeetingListDto {
         this.thumbnail = meeting.getThumbnailImage();
         this.externalService = meeting.getExternalService();
     }
+
+
 }
