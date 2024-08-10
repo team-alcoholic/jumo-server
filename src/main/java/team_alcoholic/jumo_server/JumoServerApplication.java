c84b15ab85894a8e7c2a9c771f634c609fdb5980
y-ngm-n <wogitopp@naver.com>
2024-08-10 18:44:16 +0900

    feat: 배포 환경 Swagger UI CORS 에러 관련 설정

--- src/main/java/team_alcoholic/jumo_server/JumoServerApplication.java
+++ src/main/java/team_alcoholic/jumo_server/JumoServerApplication.java
@@ -1,10 +1,15 @@
 package team_alcoholic.jumo_server;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
+@OpenAPIDefinition(servers = {
+	@Server(url = "/api/v1", description = "Default Server URL")
+})
 @EnableJpaAuditing
 @SpringBootApplication
 public class JumoServerApplication {
