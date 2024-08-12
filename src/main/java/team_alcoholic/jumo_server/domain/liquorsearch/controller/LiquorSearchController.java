2c8c100e28e7f54bbe9b69932445589c39ca5765
Youngmin Song <68019733+y-ngm-n@users.noreply.github.com>
2024-08-12 09:24:06 +0900

    [BYOB-167] 주류 데이터 구조 변경에 따른 관련 코드 수정 (#45)

--- src/main/java/team_alcoholic/jumo_server/domain/liquorsearch/controller/LiquorSearchController.java
+++ src/main/java/team_alcoholic/jumo_server/domain/liquorsearch/controller/LiquorSearchController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import team_alcoholic.jumo_server.domain.liquorsearch.domain.Liquor;
+import team_alcoholic.jumo_server.domain.liquorsearch.domain.LiquorES;
 import team_alcoholic.jumo_server.domain.liquorsearch.service.LiquorSearchService;
 
 import java.util.List;
@@ -18,7 +18,7 @@ public class LiquorSearchController {
     private final LiquorSearchService liquorService;
 
     @GetMapping()
-    public List<Liquor> searchLiquors(@RequestParam(name = "keyword") String keyword) {
+    public List<LiquorES> searchLiquors(@RequestParam(name = "keyword") String keyword) {
         return liquorService.search(keyword);
     }
 }
