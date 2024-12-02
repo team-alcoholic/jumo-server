26ff8fc510c87635a87eab7ba82bf0276a2b167f
y-ngm-n <wogitopp@naver.com>
2024-12-02 16:35:04 +0900

    fix: ElasticSearchConfig properties 사용

--- src/main/java/team_alcoholic/jumo_server/global/config/ElasticSearchConfig.java
+++ src/main/java/team_alcoholic/jumo_server/global/config/ElasticSearchConfig.java
@@ -1,5 +1,6 @@
 package team_alcoholic.jumo_server.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
@@ -10,11 +11,20 @@ import org.springframework.data.elasticsearch.repository.config.EnableElasticsea
 @EnableElasticsearchRepositories(basePackages = "team_alcoholic.jumo_server.v1.liquorsearch.repository")
 public class ElasticSearchConfig extends ElasticsearchConfiguration {
 
+    @Value("${spring.elasticsearch.uris}")
+    private String uris;
+
+    @Value("${spring.elasticsearch.username}")
+    private String username;
+
+    @Value("${spring.elasticsearch.password}")
+    private String password;
+
     @Override
     public ClientConfiguration clientConfiguration() {
         return ClientConfiguration.builder()
-            .connectedTo("localhost:9200")
-            .withBasicAuth("elastic", "1wqIio*s6Eo*ZrR4-xKx")
+            .connectedTo(uris)
+            .withBasicAuth(username, password)
             .build();
     }
 }
